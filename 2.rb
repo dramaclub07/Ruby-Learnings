17eb3bc4d459480780780a2e2d838b0977fa77ba
dramaclub07 <priyanshupositive@gmail.com>
2024-12-10 15:01:36 +0530

    [Priyanshu] ADD:-> skipped multiples of 3.(1->10)

---
+++ 2.rb
@@ -0,0 +1,7 @@
+#Create a Ruby program that loops through numbers from 1 to 20. Skip numbers that are multiples of 3 and print the rest.
+
+(1..20).each do |num|
+    if num % 3 != 0
+        puts num
+    end
+end
