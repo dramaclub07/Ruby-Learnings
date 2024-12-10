ee4de99d66d29840b7771132e5a8fbb0e2b73b97
dramaclub07 <priyanshupositive@gmail.com>
2024-12-11 00:17:18 +0530

    [Priyanshu] ADD:division/0(rescue)

---
+++ 4.rb
@@ -0,0 +1,4 @@
+#Write a ruby program that simulates a simple counter. It attempts to divide a number by a given divisor. 
+#If the divisor is 0, retry the division after prompting the user for a new divisor
+ begin
+    
