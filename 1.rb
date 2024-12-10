2db4f93778cd29a1d4b741d70bdcf63413e02dfb
dramaclub07 <priyanshupositive@gmail.com>
2024-12-10 14:46:01 +0530

    [Priyanshu] ADD: divisibilty by 7

---
+++ 1.rb
@@ -0,0 +1,26 @@
+#divisibility by 7
+numbers = gets.chomp.split.map(&:to_i) 
+numbers.each do |num|
+  if num % 7 == 0
+    break
+  else
+    puts num
+  end
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
