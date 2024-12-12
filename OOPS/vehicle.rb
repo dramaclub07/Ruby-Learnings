46e7f25c8fc337fdb866db51492fddef3858fa2e
dramaclub07 <priyanshupositive@gmail.com>
2024-12-12 14:27:00 +0530

    [Priyanshu] ADD: vehicle class implemnted

---
+++ OOPS/vehicle.rb
@@ -0,0 +1,29 @@
+class Vehicle  
+  def calculate_rent(days)  
+    raise NotImplementedError, "calculate_rent must implement this method!"  
+  end  
+end  
+
+class Car < Vehicle  
+  def calculate_rent(days)  
+    20 * days  
+  end  
+end  
+
+class Bike < Vehicle  
+  def calculate_rent(days)  
+    10 * days  
+  end  
+end  
+
+ 
+car = Car.new  
+bike = Bike.new  
+
+  
+car_rent = car.calculate_rent(5)  
+bike_rent = bike.calculate_rent(10)  
+
+  
+puts "CAR RENTED FOR 5 days: RENT per day \$20: TOTAL=> $#{car_rent}"  
+puts "BIKE RENTED FOR 10 days: RENT per day \$10:TOTAL=> $#{bike_rent}"
