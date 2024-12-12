2d67841887b489ed5570e4c38d0bf0c21b0aa826
dramaclub07 <priyanshupositive@gmail.com>
2024-12-12 17:56:35 +0530

    [Priyanshu] ADD: Email and SMS messaging

--- OOPS/messaging.rb
+++ OOPS/messaging.rb
@@ -5,5 +5,44 @@ Implement two classes EmailMessage and SMSMessage that both implement the send_m
 Write a function send_notification that takes an object of type Message and calls the send_message method.
 Demonstrate polymorphism by calling send_notification with instances of both EmailMessage and SMSMessage.
 =end
+class Message
+  def send_message
+    raise NotImplementedError, "Subclass must implement send_message"
+  end
+  
+end
 
+class EmailMessage < Message
+  def initialize(to, from, message)
+    @to = to
+    @from = from
+    @message = message
+  end
+
+  def send_message
+    puts "Sending email to #{@to} from #{@from} with message #{@message}"
+  end
+end
+
+class SMSMessage < Message
+  def initialize(to, from, message)
+    @to = to
+    @from = from
+    @message = message
+  end
+
+  def send_message
+    puts "Sending SMS to #{@to} from #{@from} with message #{@message}"
+  end
+end
+
+def send_notification(message)
+  message.send_message
+end
+
+email_message = EmailMessage.new("john@example.com", "jane@example.com", "Hello, how are you?")
+sms_message = SMSMessage.new("1234567890", "9876543210", "Hello, how are you?")
+
+send_notification(email_message)
+send_notification(sms_message)
 
