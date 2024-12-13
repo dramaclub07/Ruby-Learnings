bcbdda375124fc9751d0e5b3defbdef0d26931cb
dramaclub07 <priyanshupositive@gmail.com>
2024-12-13 11:49:14 +0530

    [Priyanshu] ADD: CALCULATOR TESTING

--- 2.rb
+++ 2.rb
@@ -1,6 +1,6 @@
 #Create a Ruby program that loops through numbers from 1 to 20. Skip numbers that are multiples of 3 and print the rest.
 
-(1..20).each do |num|
+(1..20).each do|num| 
     if num % 3 != 0
         puts num
     end
